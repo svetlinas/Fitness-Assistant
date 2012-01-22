53670b665f1b603a233e1c750db7171d66260ae7
Svetlina Shopova <m00n41ld@yahoo.com>
2012-01-22 15:33:45 +0200

    Adding first textView to BaseActivity

--- FitnessAssistant/src/bg/su/fmi/fitness/assistant/FitnessAssistantActivity.java
+++ FitnessAssistant/src/bg/su/fmi/fitness/assistant/FitnessAssistantActivity.java
@@ -2,12 +2,17 @@ package bg.su.fmi.fitness.assistant;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class FitnessAssistantActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        
+        TextView textView = new TextView(this);
+        textView.setText(R.string.app_name);
+        
+        setContentView(textView);
     }
 }
